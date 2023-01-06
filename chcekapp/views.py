da603e91e39a4399c29bfa56bbcf7ead378cd7d7
ANVAR <68984682+aanvar123456@users.noreply.github.com>
2023-01-06 18:18:50 +0530

    lo

--- chcekapp/views.py
+++ chcekapp/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+        return redirect('')
+    else:
+        return HttpResponse('dasd')
